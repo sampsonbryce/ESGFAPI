a3bd0555889c17a7094c19f06b5cf71d3c659a8d
sampsonbryce <sampson.bryce@yahoo.com>
2016-03-18 15:47:47 +0000

    everything working. building constraint

--- esgfapi.py
+++ esgfapi.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pyesgf.search import SearchConnection
+import json
 import os
 
 esgfapp = Flask(__name__)
@@ -16,14 +17,70 @@ def index():
     return render_template('index.html')
 
 
-@esgfapp.route('/search')
+@esgfapp.route('/search', methods=['POST'])
 def search():
-    print "here"
+
+    # Get Data from POST
+    args = ['project', 'query', 'model', 'experiment']
+
+    function = "conn.new_context("
+    for i, arg in enumerate(args):
+        if arg == "project":
+            value = request.json[arg].upper()
+        else:
+            value = request.json[arg]
+        if value:
+            function += "{0}='{1}',".format(arg, value)
+    if function[:-1] != "(":
+        function = function[:-1]
+    function += ')'
+
+    # SEARCH
     conn = SearchConnection(CORE_LINK, distrib=True)
-    ctx = conn.new_context(project='CMIP5', model='HadCM3', experiment='decadal2000', time_frequency='day')
-    ctx = ctx.constrain(realm='ocean', ensemble='r1i2p1')
-    data = request.form
-    return data
+    print "FUNC:", function
+    ctx = eval(function)
+    print "CTX:", ctx
+    d = ctx.__dict__
+    print ctx.hit_count
+    del d['facet_constraints']
+    del d['connection']
+    print "D:", d
+
+    print ctx.constrain.__code__.co_varnames
+    
+    j = json.dumps(d)
+    return j
+
+
+@esgfapp.route('/constrain', methods=['POST'])
+def constrain():
+    # Get Data from POST
+    args = ['project', 'query', 'model', 'experiment']
+
+    function = "conn.new_context("
+    for i, arg in enumerate(args):
+        if arg == "project":
+            value = request.json[arg].upper()
+        else:
+            value = request.json[arg]
+        if value:
+            function += "{0}='{1}',".format(arg, value)
+    if function[:-1] != "(":
+        function = function[:-1]
+    function += ')'
+
+    # SEARCH
+    conn = SearchConnection(CORE_LINK, distrib=True)
+    ctx = eval(function)
+    d = ctx.__dict__
+    del d['facet_constraints']
+    del d['connection']
+
+    # CONSTRAIN
+    print ctx.constrain.__code__.co_varnames
+
+    j = json.dumps(d)
+    return j
 
 
 if __name__ == '__main__':
--- static/scripts.js
+++ static/scripts.js
@@ -1,19 +1,42 @@
 var url = 'http://ec2-52-37-75-79.us-west-2.compute.amazonaws.com:5000/search';
-data = {hello: 'hello world'}
-$(document).ready(function() {
-    $('#searchbutton).click(function() {
-	    $.ajax({
-		    url: url,
-		    data: data,
-		    type: 'POST',
-		    statusCode: {
-			    200: function(response) {
-				    alert('success');
-			    },
-			    500: function(response) {
-				    alert('fail');
-			    }
-		    },
-	    });
+$(document).ready(function () {
+    $('#search-button').click(function () {
+        data = {};
+        data['project'] = $('#project option:selected').val();
+        data['query'] = $('#query').val();
+        data['model'] = $('#model').val();
+        data['experiment'] = $('#experiment').val();
+                $.ajax({
+            url: url,
+            data: JSON.stringify(data),
+            type: 'POST',
+            contentType: 'application/json;charset=UTF-8',
+            statusCode: {
+                200: function (response) {
+                    $('#results').empty();
+                    var elem = $('<select />');
+                    //elem = ('#results-combobox');
+                    var data = JSON.parse(response);
+                    console.log(data);
+
+                    if(data['_SearchContext__facet_counts'] == null) {
+                       $('<p />', {text: "No Results Found"}).appendTo('#results');
+                    }
+                    else {
+                        var list = data['_SearchContext__facet_counts']['cf_standard_name']
+                        console.log(list)
+
+                        for (var val in list) {
+                            console.log(val, $.type(val));
+                            $('<option />', {value: val, text: val + "\tHITS:" + list[val]}).appendTo(elem);
+                        }
+                        elem.appendTo($('#results'));
+                    }
+                },
+                500: function (response) {
+                    alert('fail');
+                }
+            },
+        });
     });
 });
